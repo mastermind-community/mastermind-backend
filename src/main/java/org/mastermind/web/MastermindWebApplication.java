974390d4f2ffaac567c0283e0a3d962f8b46699f
Ruslan Hladchenko <rugladchenko@gmail.com>
2023-12-21 14:50:29 +0200

    init

---
+++ src/main/java/org/mastermind/web/MastermindWebApplication.java
@@ -0,0 +1,13 @@
+package org.mastermind.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastermindWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MastermindWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mastermind/web/MastermindWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mastermind.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MastermindWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
